0eb8494c4d43ff8a927b274a652b91b3f0fd9344
CJ Reyes <itscjreyes@gmail.com>
2019-09-04 13:48:24 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync'); 
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify-es').default;
+const reload = browserSync.reload;
+
+gulp.task('styles', () => {
+    return gulp.src('./src/styles/**/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+        .pipe(concat('main.css'))
+        .pipe(gulp.dest('./dist/'))
+        .pipe(reload({stream:true}));
+});
+
+gulp.task('js', () => {
+    return gulp.src('./src/scripts/*.js')
+		.pipe(plumber({
+		  errorHandler: notify.onError("Error: <%= error.message %>")
+		}))
+		.pipe(uglify())
+		.pipe(gulp.dest('./dist/'))
+		.pipe(reload({stream:true}));
+});
+
+gulp.task('bs', () => {
+	return browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});
+});
+
+gulp.task('watch',() => {
+    gulp.watch('./src/**/*.js', ['js']);
+    gulp.watch('./src/**/*.scss', ['styles']);
+    gulp.watch('./dist/main.css', reload);
+    gulp.watch('./*.html',reload);
+});
+
+gulp.task('default',['styles','js','watch','bs']);
